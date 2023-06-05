aeb0e30d7ae10348c5b3930bf4e1f96faa564de9
emiliamolina942 <emiliamolina942@gmail.com>
2023-06-04 22:13:12 -0300

    Agregue monstruos

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Assets, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 860,
+	height: 1113
 });
 
-//Loader.shared.add({url:"./pngegg.png", name:"myegg"});
+Assets.add("Clampy", "./clampy.png");
+Assets.add("mymonster","./monster.png");
 
-const clampy: Sprite = Sprite.from("./clampy.png");
-//const clampy: Sprite = Sprite.from("myegg");
+Assets.load(["Clampy", "mymonster"]).then(()=>{
 
-//console.log("Hola chiquis");
+	const clampy: Sprite = Sprite.from("mymonster");
+	console.log("Holis", clampy.width, clampy.height);
+	
+	//clampy.anchor.set(0);
+	//clampy.x = 0;
+	//clampy.y = 0;
 
-clampy.anchor.set(0.5);
+	app.stage.addChild(clampy); 
 
-clampy.x = 300;
-clampy.y = 300;
-
-app.stage.addChild(clampy); 
+});
